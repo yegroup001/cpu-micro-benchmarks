592be64810efa5b76bded354d71763e9146c557e
yegroup001 <1716709048@qq.com>
2026-05-14 18:12:33 +0800

    Add cortex-a720 and x4 support

--- src/uarch.cpp
+++ src/uarch.cpp
@@ -225,6 +225,12 @@ enum uarch get_uarch_inner() {
   } else if (implementer == 0x41 && part == 0xd83) {
     fprintf(stderr, "ARM Neoverse V3 detected\n");
     return neoverse_v3;
+  } else if (implementer == 0x41 && part == 0xd81) {
+    fprintf(stderr, "ARM Cortex A720 detected\n");
+    return cortex_a720;
+  } else if (implementer == 0x41 && part == 0xd82) {
+    fprintf(stderr, "ARM Cortex X4 detected\n");
+    return cortex_x4;
   } else if (implementer == 0x48 && part == 0xd01) {
     fprintf(stderr, "Hisilicon TSV110 detected\n");
     return tsv110;
